abc0cfbe7ba77c57abd2c9bbf623f83cef200df0
Shashank Verma <shashank.verma2002@gmail.com>
2020-08-06 08:17:00 +0800

    Add few GPS libs
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- platform/dependencies.mk
+++ platform/dependencies.mk
@@ -5,3 +5,10 @@ PRODUCT_PACKAGES += \
     libmmlib2d_interface \
     libmm-qcamera \
     libqomx_core
+#GNSS HAL
+PRODUCT_PACKAGES += \
+    libgps.utils \
+    libgnss \
+    liblocation_api \
+    android.hardware.gnss@1.0-impl-qti \
+    android.hardware.gnss@1.0-service-qti
