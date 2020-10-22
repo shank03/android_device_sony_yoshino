04c44409e60d39809e01153eae3265c46df68239
derfelot <derfelot.git@gmail.com>
2020-10-22 12:42:05 +0800

    yoshino: Merge common into platform dir

--- platform/hardware.mk
+++ platform/hardware.mk
@@ -1,7 +1,36 @@
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 ### AUDIO
 PRODUCT_PACKAGES += \
     libvolumelistener \
     bthost_if
+
+PRODUCT_PACKAGES += \
+    audio.r_submix.default \
+    audio.usb.default
+
+PRODUCT_PACKAGES += \
+    audio.a2dp.default \
+    audio.bluetooth.default \
+    android.hardware.bluetooth.a2dp@1.0 \
+    android.hardware.bluetooth.audio@2.0 \
+    android.hardware.bluetooth.audio@2.0-impl
+
+### CHARGER
+PRODUCT_PACKAGES += \
+    charger_res_images
 
 ### COVER
 PRODUCT_PACKAGES += \
@@ -29,6 +58,12 @@ PRODUCT_PACKAGES += \
 ### NETWORKSWITCHER APK
 PRODUCT_PACKAGES += \
     NetworkSwitcher
+
+### NFC
+PRODUCT_PACKAGES += \
+    com.android.nfc_extras \
+    NfcNci \
+    Tag
 
 ### REMOVE UNWANTED PACKAGES
 PRODUCT_PACKAGES += \
@@ -135,6 +170,15 @@ PRODUCT_PACKAGES += \
     vr.msm8998 \
     android.hardware.vr@1.0-impl \
     android.hardware.vr@1.0-service
+
+### WIFI
+PRODUCT_PACKAGES += \
+    hostapd \
+    libwpa_client \
+    p2p_supplicant.conf \
+    wificond \
+    wpa_supplicant \
+    wpa_supplicant.conf
 
 # USB
 PRODUCT_PACKAGES += \
