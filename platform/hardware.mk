761e2d227ac8a97c0698394cafead0b81fff355f
Shashank Verma <shashank.verma2002@gmail.com>
2020-11-19 12:37:36 +0800

    hardware: Remove NetworkSwitcher
    A modified customization selector replaces the need of switching/toggling network
    
    So long partner :)
    
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- platform/hardware.mk
+++ platform/hardware.mk
@@ -59,10 +59,6 @@ PRODUCT_PACKAGES += \
     libipanat \
     liboffloadhal
 
-### NETWORKSWITCHER APK
-PRODUCT_PACKAGES += \
-    NetworkSwitcher
-
 ### XPERIAPARTS APK
 PRODUCT_PACKAGES += \
     XperiaParts
