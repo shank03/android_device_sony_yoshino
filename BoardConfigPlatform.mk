19b0c2a0a5d0393c8d7107d5c8b383c1ac3dd441
derfelot <derfelot.git@gmail.com>
2021-02-01 23:51:41 +0100

    yoshino-common: Drop in-tree twrp support
    It hasn't worked inside Lineage sources for a while.

--- BoardConfigPlatform.mk
+++ BoardConfigPlatform.mk
@@ -146,9 +146,9 @@ BOARD_VENDOR_SEPOLICY_DIRS += device/sony/yoshino-common/sepolicy/vendor
 
 ### RECOVERY
 ifneq ($(filter maple maple_dsds, $(TARGET_DEVICE)),)
-TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/ramdisk/fstab_maple.recovery
+TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/recovery/fstab_maple.recovery
 else
-TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/ramdisk/fstab.recovery
+TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/recovery/fstab.recovery
 endif
 
 ### PROPS
@@ -170,7 +170,3 @@ BUILD_BROKEN_DUP_RULES := true
 
 ### BUILD_COPY_HEADERS ALLOWED
 BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
-
-ifeq ($(WITH_TWRP),true)
--include $(PLATFORM_PATH)/twrp.mk
-endif
