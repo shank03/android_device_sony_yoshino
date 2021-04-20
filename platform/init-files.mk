e4b32bcc2f6097534cb533930dc6a2b8b1fb7081
Shashank Verma <shashank.verma2002@gmail.com>
2021-04-20 20:30:55 +0800

    yoshino-common: platform: Remove unused fstab copy cmd
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- platform/init-files.mk
+++ platform/init-files.mk
@@ -6,11 +6,6 @@ PRODUCT_COPY_FILES += \
     $(PLATFORM_PATH)/config/init/init.yoshino.srv.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.yoshino.srv.rc \
     $(PLATFORM_PATH)/config/init/init.yoshino.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.yoshino.usb.rc
 
-# fstab
-PRODUCT_COPY_FILES += \
-    $(PLATFORM_PATH)/config/init/idd.fstab:$(TARGET_COPY_OUT_VENDOR)/etc/idd.fstab \
-    $(PLATFORM_PATH)/config/init/qns.fstab:$(TARGET_COPY_OUT_VENDOR)/etc/qns.fstab
-
 # init.qcom.early_boot.sh (modified)
 PRODUCT_COPY_FILES += \
     $(PLATFORM_PATH)/config/init/init.qcom.early_boot.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qcom.early_boot.sh \
